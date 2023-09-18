849b6de435249981ea1fd6072efc1557c4b185cc
adharsh <adharshd100@gmail.com>
2023-09-18 23:58:55 +0530

    public games listing page created

---
+++ client/src/scenes/MyGames.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyGames = () => {
+  return (
+    <div>MyGames</div>
+  )
+}
+
+export default MyGames
--- client/src/scenes/PublicGames.tsx
+++ client/src/scenes/PublicGames.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+const games: number[] = [1,2,3,4,5,6,7,9,10,11,12,14,14,15,1,3,34,3]
+
 const PublicGames: React.FC = () => {
   return (
-    <div>PublicGames</div>
+    <div className='public'>
+      <div className="public__body">
+          <p>My Games</p>
+          <div className="public__mygames">
+            {
+              games.map((item: number, val: number) => {
+                return(
+                  <div className='public__mygames--item' key={val}>
+                   {item}
+                  </div>
+                )
+              })
+            }
+          </div>
+
+            <p>Public Games</p>
+          <div className="public__listedgames">
+            {
+              games.map((item: number, val: number) => {
+                return(
+                  <div className="public__listedgames--item">
+                    {item}
+                  </div>
+                )
+              })
+            }
+          </div>
+      </div>
+    </div>
   )
 }
 
